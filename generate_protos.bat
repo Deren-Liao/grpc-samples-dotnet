9e37a1df91ee4ead3a01121b8099e36266393d81
Deren <derenl@google.com>
2017-05-26 03:49:19 -0700

    Add helloworld client

--- generate_protos.bat
+++ generate_protos.bat
@@ -44,4 +44,8 @@ set TOOLS_PATH=packages\Grpc.Tools.1.0.1\tools\windows_x64
 
 %TOOLS_PATH%\protoc.exe Chat\protos\chat.proto --grpc_out Chat --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
 
+%TOOLS_PATH%\protoc.exe HelloWorldClient\helloworld.proto --csharp_out HelloWorldClient
+
+%TOOLS_PATH%\protoc.exe HelloWorldClient\helloworld.proto  --grpc_out HelloWorldClient --plugin=protoc-gen-grpc=%TOOLS_PATH%\grpc_csharp_plugin.exe
+
 endlocal
